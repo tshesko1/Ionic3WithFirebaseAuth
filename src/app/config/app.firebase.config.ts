8de2d3cb6d8a94a29f90b0adb9d8cb2c7df4ff60
Francis Missumbi <tshesko1@gmail.com>
2018-05-16 14:37:44 +0530

    add your own credentials in src/app/config/app.firebase.config.ts from console.firebase.google.com

---
+++ src/app/config/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: 				"KINDLY",
+  authDomain: 				"ADD",
+  databaseURL:				"YOUR",
+  projectId: 				"OWN",
+  storageBucket: 			"CREDENTIALS",
+  messagingSenderId: 	"HERE : console.firebase.google.com"
+};
